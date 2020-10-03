356d08437fe3ee3a97d8ab3052561b10d7b343b6
Denys Golubiev <dgo@adorsys.de>
2020-10-03 19:22:19 +0200

    First version of script

---
+++ sync_repo.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+
+GITHUB="git@github.com"
+UPSTREAM_ORG="DG0lden"
+ORIGIN_ORG="golubiev"
+REPO="git-forks-sync"
+
+UPSTREAM_URL=${GITHUB}:${UPSTREAM_ORG}/${REPO}.git
+ORIGIN_URL=${GITHUB}:${ORIGIN_ORG}/${REPO}.git
+
+echo "upstream is: ${UPSTREAM_URL}"
+echo "origin is: ${ORIGIN_URL}"
+
+#1. Зклонувати в нову папку форк без локального клона
+TMPDIR=${TMP:-/tmp}/$(date +%Y-%d-%m-%H%M)-${REPO}
+echo $TMPDIR
+mkdir -p $TMPDIR
+cd $TMPDIR
+git clone ${ORIGIN_URL} ${REPO}
+cd ${REPO}
+
+#2. Підключити оригінал как апстрім
+git remote add upstream ${UPSTREAM_URL}
+
+#3. Зафетчити апстрім
+git fetch origin
+git fetch upstream
+
+#4. Пройтись циклом по бранчам апстріма
+for brname in `git branch -r | grep upstream | grep -v master | grep -v HEAD | sed -e 's/.*\///g'`;
+do
+	#4.1. Створити локальну бранчу із апстріма
+	echo "Using ${brname}"
+
+	#4.2. Перевірити чи є така бранча на оріджині
+	if git branch --track ${brname} origin/${brname} ; then
+		#4.4. Якщо є - змерджити з апстрімом по фф
+		echo "${brname} exists on origin, trying fast-forward merge"
+		git checkout ${brname}
+		if git merge --fast-forward upstream/${brname} ; then
+			#4.6. Якщо фф пройшов запушати результат в ориджін
+			echo "${brname}: fast-forward merge successfully done"
+		else
+			#4.5. Якщо фф не пройшов - записати в список на локальний мердж
+			echo "${brname}: fast-forward merge failed, writing down branch to error list"
+			ERROR_LIST="${ERROR_LIST} ${brname}"
+		fi
+	else
+		#4.3. Якщо немає, запушати її в ориджін
+		echo "${brname} doesn't exist on origing, checking it out from upstream"
+		git status
+		set -x
+		git checkout -b ${brname} upstream/${brname}
+		git push -u origin
+		git branch --set-upstream-to=origin/${brname}
+		set +x
+	fi
+	echo "${brname} done\n\n"
+done
+
+echo "All branches are done, pushing result to origin"
+#git push --all --tags origin
+#5. Видалити локальну репу
+rm -rf ${TMPDIR}
+
